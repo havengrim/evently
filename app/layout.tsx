9f73fc479825835632d18ad7b767bfcc595eb5f2
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-01-08 16:04:32 +0800

    backend integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter, Poppins } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 
